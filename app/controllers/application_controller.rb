a226b20e2c292ff5ce8df489c7a4bb5cb14487eb
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-26 05:57:45 +0000

    deviseのstrong parameter＆ビュー作成と修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :confiure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  #ユーザ登録時にユーザ名のデータ操作を許可
+  def confiure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
