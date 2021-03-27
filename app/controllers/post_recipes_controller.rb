69223f2431d48dbed777efa6057bc69b5e44ab51
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-27 02:35:41 +0000

    レシピ詳細画面(show)作成 & showアクションをコントローラに定義

--- app/controllers/post_recipes_controller.rb
+++ app/controllers/post_recipes_controller.rb
@@ -14,6 +14,7 @@ class PostRecipesController < ApplicationController
   end
 
   def show
+    @post_recipe = PostRecipe.find(params[:id])
   end
 
   def destroy
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0487005cf47ec7e5d1dfe66df0204b5ba525f2e79fac4b8cf56ed0389dbf3728c92fa73eb80f8ffb1c5e0fa09e8198e10f1c2e1f2ef036fcfa050cfdc1a40375'
