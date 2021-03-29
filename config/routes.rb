e0f23ccae2d1dc72795aba24162f2279abd2d720
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-29 11:37:41 +0000

    Aboutページとルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#index'
 
-  resources :post_recipes, only: [:new, :create, :index, :show, :destroy]
-
-  resources :users, only: [:show, :edit, :update] do
-    
+  resources :homes, only: [:index] do
+    collection do
+      get :about
+    end
   end
+
+  resources :post_recipes, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
 end
