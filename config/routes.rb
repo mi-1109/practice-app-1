12cd2a9354fdd470a31463fc9ab498d07b257755
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-29 11:27:28 +0000

    homes_controllerのtopアクションを、indexアクションに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: 'homes#top'
+  root to: 'homes#index'
 
   resources :post_recipes, only: [:new, :create, :index, :show, :destroy]
 
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update] do
+    
+  end
 end
